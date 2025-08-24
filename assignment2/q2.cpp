ea7e7c084f7c55bfb62504a1bd1bb70b9bec0590
Aryan Juneja <aryanjuneja2020@gmail.com>
2025-08-25 00:29:40 +0530

    Create q2.cpp

---
+++ assignment2/q2.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+void bubble_sort(int arr[],int n){
+    
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n-i-1; j++)
+        {
+            if(arr[j]>arr[j+1]){
+                swap(arr[j+1],arr[j]);
+            }
+        }
+        
+    }
+    
+    return ;
+}
+int main(){
+    int arr[] = {64, 34 ,25 ,12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    bubble_sort(arr,n);
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+    
+    return 0;
+}
